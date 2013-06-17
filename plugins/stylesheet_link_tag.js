dce4321e553c4b2dbe339e6ad52ea70fa909724a
Duncan Beevers <duncan@dweebd.com>
2013-06-16 22:44:08 -0500

    Manually setting up styles

--- plugins/stylesheet_link_tag.js
+++ plugins/stylesheet_link_tag.js
@@ -1,9 +1,26 @@
-module.exports = function(wintersmith, callback) {
-  var stylesView = function(env, locals, contents, templates, callback) {
-    console.log("Apparently rendering the styles view?");
-    callback();
+module.exports = function(env, callback) {
+
+  env.helpers.stylesheet_link_tag = function() {
+    var args = Array.prototype.slice.call(arguments);
+
+    function flatten(o, src, map) {
+      for (var i = 0, len = src.length; i < len; i++) {
+        if (Array.isArray(src[i])) {
+          arguments.callee(o, src[i], map);
+        } else {
+          o.push(map(src[i]));
+        }
+      }
+
+      return o;
+    }
+
+    var o = flatten([], args, function(arg) {
+      return "<link rel=\"" + arg + "\" type=\"text/css\"></link>";
+    });
+
+    return o.join("");
   };
 
-  wintersmith.registerView("styles", stylesView);
   callback();
 };
